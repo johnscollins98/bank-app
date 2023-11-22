d0f61b838cc70dcd1262a0425ce0c3e98ccfabe8
John Collins <johnscollins98@gmail.com>
2023-11-22 20:41:50 +0000

    place spending summary in a carousel

---
+++ src/app/categories.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import { Transactions } from '@/lib/starling-types';
+import { Accordion, AccordionItem, Chip } from '@nextui-org/react';
+import Link from 'next/link';
+
+interface Props {
+  searchParams: Record<string, string>;
+  transactions: Transactions;
+}
+
+export default function Categories({ searchParams, transactions }: Props) {
+  const { filterBy } = searchParams;
+
+  const total = transactions.feedItems.reduce(
+    (total, transaction) => {
+      const value =
+        transaction.direction === 'IN'
+          ? transaction.amount.minorUnits
+          : -1 * transaction.amount.minorUnits;
+      const category = transaction.spendingCategory;
+
+      return {
+        ...total,
+        [category]: (total[category] ?? 0) + value,
+        total: total.total + value,
+      };
+    },
+    { total: 0 } as Record<string, number>
+  );
+
+  return (
+    <Accordion>
+      <AccordionItem title="Spending Summary">
+        <div className="flex gap-2 flex-wrap">
+          <Link
+            href={`.?${new URLSearchParams({ ...searchParams, filterBy: '' }).toString()}`}
+            passHref
+          >
+            <Chip
+              className="hover:bg-blue-600 transition-colors duration-25 text-xs"
+              color={filterBy === '' ? 'primary' : 'default'}
+            >
+              <div className="flex gap-2">
+                <div>Total</div>
+                <div>£{total.total / 100}</div>
+              </div>
+            </Chip>
+          </Link>
+          {Object.keys(total)
+            .filter((k) => k !== 'total')
+            .sort((a, b) => total[a] - total[b])
+            .map((key) => (
+              <Link
+                passHref
+                href={`.?${new URLSearchParams({
+                  ...searchParams,
+                  filterBy: filterBy === key ? '' : key,
+                })}`}
+                key={key}
+                color={key === filterBy ? 'primary' : 'default'}
+              >
+                <Chip
+                  color={key === filterBy ? 'primary' : 'default'}
+                  className="hover:bg-blue-600 transition-colors duration-25 text-xs"
+                >
+                  <div className="flex gap-2">
+                    <div className="capitalize">{key.replaceAll('_', ' ').toLocaleLowerCase()}</div>
+                    <div className={total[key] > 0 ? 'text-green-500' : ''}>
+                      £
+                      {(total[key] / 100).toLocaleString(undefined, {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2,
+                      })}
+                    </div>
+                  </div>
+                </Chip>
+              </Link>
+            ))}
+        </div>
+      </AccordionItem>
+    </Accordion>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Starling } from '@/lib/starling-api-service';
-import { Button, ButtonGroup, Chip } from '@nextui-org/react';
-import Link from 'next/link';
-import TransactionTable from './table';
+import { Button, ButtonGroup } from '@nextui-org/react';
+import Categories from './categories';
 
 function lastDayOfMonth(dayIndex: number, year: number, month: number) {
   var lastDay = new Date(year, month + 1, 0);
@@ -39,23 +38,6 @@ export default async function Home({ searchParams }: { searchParams: Record<stri
   const createRedirectLink = (newOffset: number) =>
     new URLSearchParams({ ...searchParams, offset: newOffset.toString() }).toString();
 
-  const total = transactions.feedItems.reduce(
-    (total, transaction) => {
-      const value =
-        transaction.direction === 'IN'
-          ? transaction.amount.minorUnits
-          : -1 * transaction.amount.minorUnits;
-      const category = transaction.spendingCategory;
-
-      return {
-        ...total,
-        [category]: (total[category] ?? 0) + value,
-        total: total.total + value,
-      };
-    },
-    { total: 0 } as Record<string, number>
-  );
-
   return (
     <main className="dark flex h-[100dvh] flex-col p-4 gap-4 overflow-hidden">
       <div className="flex gap-2 items-center justify-between">
@@ -72,55 +54,8 @@ export default async function Home({ searchParams }: { searchParams: Record<stri
           </Button>
         </ButtonGroup>
       </div>
-      <div className='font-bold'>
-      Balance: £ {balance.effectiveBalance.minorUnits / 100}
-      </div>
-      <div className="flex gap-2 flex-wrap">
-        <Link
-          href={`.?${new URLSearchParams({ ...searchParams, filterBy: '' }).toString()}`}
-          passHref
-        >
-          <Chip
-            className="hover:bg-blue-600 transition-colors duration-25 text-xs"
-            color={filterBy === '' ? 'primary' : 'default'}
-          >
-            <div className="flex gap-2">
-              <div>Total</div>
-              <div>£{total.total / 100}</div>
-            </div>
-          </Chip>
-        </Link>
-        {Object.keys(total)
-          .filter((k) => k !== 'total')
-          .sort((a, b) => total[a] - total[b])
-          .map((key) => (
-            <Link
-              passHref
-              href={`.?${new URLSearchParams({
-                ...searchParams,
-                filterBy: filterBy === key ? '' : key,
-              })}`}
-              key={key}
-              color={key === filterBy ? 'primary' : 'default'}
-            >
-              <Chip
-                color={key === filterBy ? 'primary' : 'default'}
-                className="hover:bg-blue-600 transition-colors duration-25 text-xs"
-              >
-                <div className="flex gap-2">
-                  <div className="capitalize">{key.replaceAll('_', ' ').toLocaleLowerCase()}</div>
-                  <div className={total[key] > 0 ? 'text-green-500' : ''}>
-                    £
-                    {(total[key] / 100).toLocaleString(undefined, {
-                      minimumFractionDigits: 2,
-                      maximumFractionDigits: 2,
-                    })}
-                  </div>
-                </div>
-              </Chip>
-            </Link>
-          ))}
-      </div>
+      <div className="font-bold">Balance: £ {balance.effectiveBalance.minorUnits / 100}</div>
+      <Categories searchParams={searchParams} transactions={transactions} />
       <div className="flex flex-1 flex-col overflow-auto">
         {feedItems.map((feedItem) => (
           <div key={feedItem.feedItemUid} className="p-3 border-t border-b border-gray-600">
