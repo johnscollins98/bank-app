d71226e2fc0442774583aa2f425d22f129dccd24
John Collins <johnscollins98@gmail.com>
2023-11-29 13:05:50 +0000

    fix date sorting of transactions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default async function Home({ searchParams }: { searchParams: Record<stri
   const transactions = await starling.getTransactions(accountId, lastThursday, lastWednesday);
   const feedItems = transactions.feedItems.filter(
     (item) => filterBy === '' || item.spendingCategory === filterBy
-  ).reverse();
+  ).sort((a, b) => Date.parse(b.transactionTime) - Date.parse(a.transactionTime));
 
   const createRedirectLink = (newOffset: number) =>
     new URLSearchParams({ ...searchParams, offset: newOffset.toString() }).toString();
