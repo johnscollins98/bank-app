2721705f7e5e7008f34aeae4babc9ec1da03ee2d
John Collins <johnscollins98@gmail.com>
2024-04-11 20:49:15 +0100

    add optimistic update for category update

--- src/app/_components/feed-entry.tsx
+++ src/app/_components/feed-entry.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { SPENDING_CATEGORIES, Transactions } from '@/lib/starling-types';
+import { SPENDING_CATEGORIES, SpendingCategory, Transactions } from '@/lib/starling-types';
 import {
   Dropdown,
   DropdownTrigger,
@@ -11,21 +11,29 @@ import {
 import DateDisplay from './date';
 import TimeDisplay from './time';
 import setCategory from '@/lib/actions/set-category';
+import { useOptimistic } from 'react';
 
 interface Props {
   feedItem: Transactions['feedItems'][number];
 }
 
 export default function FeedEntry({ feedItem }: Props) {
+  const [optimisticFeedItem, updateOptimisticFeedItem] = useOptimistic(feedItem, (_state, newFeedItem: typeof feedItem) => newFeedItem)
+
+  const updateCategoryHandler = async (c: SpendingCategory) => {
+    updateOptimisticFeedItem({ ...feedItem, spendingCategory: c });
+    await setCategory(c, feedItem.feedItemUid);
+  } 
+
   return (
     <Dropdown className="dark">
       <DropdownTrigger>
         <div className="p-3 border-t border-b border-gray-600">
           <div className="flex justify-between">
-            <div className="font-bold">{feedItem.counterPartyName}</div>
-            <div className={`font-bold ${feedItem.direction === 'IN' && 'text-blue-400'}`}>
-              {feedItem.direction === 'IN' && '+'}£
-              {(feedItem.amount.minorUnits / 100).toLocaleString(undefined, {
+            <div className="font-bold">{optimisticFeedItem.counterPartyName}</div>
+            <div className={`font-bold ${optimisticFeedItem.direction === 'IN' && 'text-blue-400'}`}>
+              {optimisticFeedItem.direction === 'IN' && '+'}£
+              {(optimisticFeedItem.amount.minorUnits / 100).toLocaleString(undefined, {
                 minimumFractionDigits: 2,
                 maximumFractionDigits: 2,
               })}
@@ -34,13 +42,13 @@ export default function FeedEntry({ feedItem }: Props) {
           <div className="flex justify-between text-xs text-gray-400">
             <div className="flex gap-3">
               <div className="capitalize font-bold">
-                {feedItem.spendingCategory.replaceAll('_', ' ').toLowerCase()}
+                {optimisticFeedItem.spendingCategory.replaceAll('_', ' ').toLowerCase()}
               </div>
-              <div>{feedItem.reference}</div>
+              <div>{optimisticFeedItem.reference}</div>
             </div>
             <div>
-              <DateDisplay date={new Date(feedItem.transactionTime)} />,{' '}
-              <TimeDisplay date={new Date(feedItem.transactionTime)} />
+              <DateDisplay date={new Date(optimisticFeedItem.transactionTime)} />,{' '}
+              <TimeDisplay date={new Date(optimisticFeedItem.transactionTime)} />
             </div>
           </div>
         </div>
@@ -48,7 +56,7 @@ export default function FeedEntry({ feedItem }: Props) {
       <DropdownMenu className="max-h-[50vh] overflow-auto" aria-label="category-menu">
         <DropdownSection title="Choose new category">
           {SPENDING_CATEGORIES.map((c) => (
-            <DropdownItem className="capitalize" key={c} onClick={() => setCategory(c, feedItem.feedItemUid)}>
+            <DropdownItem className="capitalize" key={c} onClick={() => updateCategoryHandler(c)}>
               {c.replaceAll('_', ' ').toLocaleLowerCase()}
             </DropdownItem>
           ))}
