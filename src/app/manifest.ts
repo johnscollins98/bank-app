c0cdaf6edb786703463ae4f4c486f89a6cc44ba5
John Collins <johnscollins98@gmail.com>
2024-05-05 19:11:41 +0100

    remove out of date theme in manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,7 +7,5 @@ export default function manifest(): MetadataRoute.Manifest {
     description: "Bank App",
     start_url: "/",
     display: "standalone",
-    background_color: "rgb(30 41 59)",
-    theme_color: "rgb(30 41 59)",
   };
 }
