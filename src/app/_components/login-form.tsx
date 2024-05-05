4ffb3e57489311004c1ed8eeb6ca454a95284467
John Collins <johnscollins98@gmail.com>
2024-05-05 18:31:38 +0100

    center login form

--- src/app/_components/login-form.tsx
+++ src/app/_components/login-form.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react";
 
 export default function LoginForm() {
   return (
-    <div className="flex flex-1 flex-col gap-5 items-center justify-center">
+    <div className="h-dvh flex flex-1 flex-col gap-5 items-center justify-center">
       Please sign in to use this application.
       <Button onClick={() => signIn("google")}>Sign in</Button>
     </div>
