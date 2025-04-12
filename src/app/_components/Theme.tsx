6de566315f1219f8de6b3d33b60ecb867e41ab2f
John Collins <johnscollins98@gmail.com>
2025-04-12 15:24:06 +0000

    minor dependency upgrades

--- src/app/_components/Theme.tsx
+++ src/app/_components/Theme.tsx
@@ -6,7 +6,10 @@ import { PropsWithChildren } from "react";
 export const Theme = ({ children }: PropsWithChildren) => {
   const { systemTheme } = useTheme();
   return (
-    <div className={`${systemTheme} bg-background text-foreground`}>
+    <div
+      className={`${systemTheme} bg-background text-foreground`}
+      suppressHydrationWarning
+    >
       {children}
     </div>
   );
